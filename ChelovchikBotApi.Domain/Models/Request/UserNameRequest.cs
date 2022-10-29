b417ed03a595582078681f4c3d152492149c3773
C4e10VeK <tank.t3445@gmail.com>
2022-10-30 02:58:48 +1000

    delete unused models

--- ChelovchikBotApi.Domain/Models/Request/UserNameRequest.cs
+++
@@ -1,9 +0,0 @@
-using Newtonsoft.Json;
-
-namespace ChelovchikBotApi.Domain.Models.Request;
-
-public class UserNameRequest
-{
-    [JsonProperty("username")]
-    public string? Username { get; set; }
-}
